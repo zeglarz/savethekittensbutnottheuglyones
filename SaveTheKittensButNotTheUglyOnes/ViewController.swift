4168084197d6ea045870563ca11fab840bb08da7
Konrad <konrad@konrads-macbook-pro.local>
2017-07-03 18:27:37 +0200

    Initial Commit

---
+++ SaveTheKittensButNotTheUglyOnes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittensButNotTheUglyOnes
+//
+//  Created by Konrad on 03/07/2017.
+//  Copyright © 2017 Konrad Rudnicki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
